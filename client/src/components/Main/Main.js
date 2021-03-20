64a9ff1943dfe2a09bb95846b23bdc11576d4d20
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2021-03-20 17:34:08 +0200

    Add main container

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+// import {Fragment} from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main className="main-container">
+            <h1>Sooooooome Heading</h1>
+            <p>some posts</p>
+        </main>
+    );
+}
+
+export default Main;
