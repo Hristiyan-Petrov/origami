9db73e1788d5780125b6bbdf5d91f9593a80e0b1
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2021-03-20 18:38:50 +0200

    Add posts components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { Component } from 'react';
+
+import * as postService from './services/postService';
+
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Main from './components/Main';
@@ -18,6 +21,21 @@ import style from './App.module.css';
 // }
 
 class App extends Component {
+   constructor(props) {
+      super (props);
+
+      this.state = {
+         posts: []
+      }
+   }
+
+   componentDidMount() {
+      postService.getAll()
+         .then(posts => {
+            this.setState({posts})
+         });
+   }
+
    render() {
       return (
          <div className={style.app}>
@@ -25,10 +43,10 @@ class App extends Component {
             <div className={style.container}>
                <Menu />
 
-               <Main />
+               <Main posts={this.state.posts}/>
             </div>
          </div>
-      )
+      );
    }
 }
 
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,11 +1,25 @@
 // import {Fragment} from 'react';
+import Post from '../Post';
 import './Main.css';
 
-const Main = () => {
+const Main = ({
+    posts
+}) => {
+    console.log(posts);
     return (
         <main className="main-container">
             <h1>Sooooooome Heading</h1>
-            <p>some posts</p>
+
+            <div className="posts">
+                {posts.map(x =>
+                    <Post
+                        key={x.id}
+                        content={x.content}
+                        author={x.author}
+                    />
+                )}
+            </div>
+
         </main>
     );
 }
